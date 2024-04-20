72283caa3647c83016ca49c2126c927a29de37e5
satsuki29 <sgyoten@ucmerced.edu>
2024-04-20 06:44:20 -0700

    commit

--- POLI211/poster/poster_apr.R
+++ POLI211/poster/poster_apr.R
@@ -28,6 +28,7 @@ View(df)
 names(df)
 head(df)
 glimpse(df)
+cor(df)
 #################################################
 scat1<- ggplot(data =df,
                mapping = aes(x= aircraft ,y= total_coverage))
@@ -59,4 +60,25 @@ scat3<- scat3 +
        title= "年と報道")
 print(scat3)
 #################################################
+df_new <- df[,-which(names(df) == "country_RU")]
+df_new1 <- rename(df_new, country_no = "country number",
+                  military = " military/ else(情報収集機)",
+                  invasion = "invasion（領海侵犯報告）")
 
+lm_viftest<- lm(total_coverage ~ aircraft+year+country_no+country_CN+military+Year_2022+invasion+Yomiuri+Nikkei,data=df_new1)
+vif(lm_viftest)
+#invasion、Yomiuri、Nikkei が、モデルの精度に悪影響を与えている可能性があります。
+lm_viftest2<- lm(total_coverage ~ aircraft+year+country_no+country_CN+military+Year_2022+Yomiuri,data=df_new1)
+#invasionとNikkeiを抜く
+> vif(lm_viftest2)
+
+#check heterosked~~(不均一分散)
+library("tidyverse")
+
+res_plt<- tibble(res = lm_viftest2$residuals,
+                 fitted = lm_viftest2$ fitted.values) %>%
+  ggplot(aes( x= fitted, y= res))+
+  geom_point() +
+  geom_hline(yintercept = 0)+
+  labs ( x= " fitted values", y= "residuals")
+print (res_plt)
